a12deb1a742b5d8ebc750c50a2c4b7c09372c8d6
Matt Gambogi <m@gambogi.com>
2013-09-30 01:26:57 -0400

    Made code more idiomatic

--- pacecalc.pl
+++ pacecalc.pl
@@ -4,49 +4,60 @@ use strict;
 use warnings; 
 use v5.10;
 
-sub getDistance {
+sub get_distance {
     my ($time, $pace) = @_;
-    my $dist = toSeconds($time)/toSeconds($pace);
+    my $dist = to_seconds($time)/to_seconds($pace);
     return sprintf("%.2f mi", $dist);
 }
 
-sub getPace {
+sub get_pace {
     my ($time, $dist) = @_;
-    my $pace = toSeconds($time)/$dist;
-    return toTime($pace) . " min/mi";
+    my $pace = to_seconds($time)/$dist;
+    return to_time($pace) . " min/mi";
 }
 
-sub getTime {
+sub get_time {
     my ($dist, $pace) = @_;
-    my $time = $dist*toSeconds($pace);
+    my $time = $dist*to_seconds($pace);
     return toTime($time);
 }
 
-sub toSeconds {
-        my $t = shift;
-        if (isTime($t)){
+sub to_seconds {
+    my $t = shift;
+        if (is_time($t)){
             my @time = split(/:/, $t);
             my $secs = $time[0]*60+$time[1];
             return $secs;
         }
 }
 
-sub toTime {
+sub usage {
+    say('Useage\n' .
+        'pacecalc {MM:SS | distance} {MM:SS | distance}');       
+}
+
+sub to_time {
     my $time = shift;
     return sprintf( "%d:%02d", int($time/60),($time/60 - int($time/60))*60);
 }
 
-sub isTime {
+sub is_time {
     my $t = shift;
-    return ($t =~ /([0-9]|[0-9][0-9]):[0-5][0-9]/)
+    return ($t =~ /(?:[0-9]|[0-9][0-9]):[0-5][0-9]/)
 }
 
-if (isTime($ARGV[0]) && isTime($ARGV[1])){
-    say getDistance($ARGV[0], $ARGV[1]);
+if (@ARGV == 0){
+    usage();
 }
-elsif (isTime($ARGV[0])){
-    say getPace($ARGV[0], $ARGV[1]);
+elsif (is_time($ARGV[0]) and is_time($ARGV[1])){
+    say get_distance($ARGV[0], $ARGV[1]);
 }
-elsif (isTime($ARGV[1])){
-    say getTime($ARGV[0], $ARGV[1]);
+elsif (is_time($ARGV[0])){
+    say get_pace($ARGV[0], $ARGV[1]);
+}
+elsif (is_time($ARGV[1])){
+    say get_time($ARGV[0], $ARGV[1]);
+}
+else{
+    usage();
 }
